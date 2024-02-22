3a0f8d71304342f360a6db4907aaee6289d1b381
ndeke254 <musumbijefferson@gmail.com>
2024-02-22 03:16:39 +0300

    end div for js

--- user_timeline.R
+++ user_timeline.R
@@ -1,21 +1,43 @@
 user_timeline_modal <- function(reg_no){
  shiny::modalDialog(
+  includeScript("www/app.js"),
+  includeCSS("www/styles.css"),
+  useWaiter(),
   title = strong("Actions Timeline"),
-  tags$div(actionButton(inputId = "ex",
-                        label = "",
-                        icon = icon("circle-xmark"),
-                        )
-           ),
-  size = "l",
+  fixedPanel(
+   right = 350,
+   top = 10,
+   tags$div(
+    actionButton(
+     inputId = "ex",
+     label = "",
+     icon = icon("circle-xmark"),
+    )
+   )
+  ),
+  size = "m",
   easyClose = FALSE,
   footer = NULL,
   box(
-   style = "height: 425px; overflow-y: auto;",
    title = reg_no,
    solidHeader = TRUE,
    width = 12,
-   uiOutput("timeline_block")
-  ) 
- )|>
+   div(
+    style = "
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: center;",
+    div(
+    id = "end",
+    style = "height: 242px; width: 384px;"
+   ),
+   shinyjs::hidden(
+   uiOutput("empty")
+   )
+   )
+   ) 
+  )|>
   shiny::showModal()
 }
