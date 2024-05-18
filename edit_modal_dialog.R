fbcc3fa4b1d7c004d83a95d0be09bfd45534d6ff
ndeke254 <musumbijefferson@gmail.com>
2024-05-19 00:47:43 +0300

    Removed splitLayout

--- edit_modal_dialog.R
+++ edit_modal_dialog.R
@@ -2,43 +2,38 @@ edit_modal_dialog <- function() {
  shiny::modalDialog(
   title = "Edit Student Data",
   div(
-   splitLayout(
-    cellWidths = c("18.286%","8.286%","9.286%", # nolint
-                   "5.286%","12.286%","24.286%","0%","2.286%"
-    ),
+   style = "display: flex; flex-wrap: nowrap;
+     align-items: center; gap: 5px;",
     textInput("edit_name", 
               "Name", 
-              width = "225px"),
+              width = "500px"),
     selectInput("edit_gender",
                 "Gender", 
                 choices = c("Male", "Female"),
-                width = "100px",
+                width = "150px",
                 selected = NULL),
     numericInput("edit_ID", "ID",
                  value = "", 
-                 width = "110px"),
+                 width = "150px"),
     selectInput("edit_code",
                 "Code",
                 choices = c("X74", "X75"),
-                width = "55px",
+                width = "100px",
                 selected = NULL),
     disabled(
      textInput("edit_reg",
                "Registration Number",
                value = "",
-               width = "150px")
+               width = "300px")
     ),
     fileInput("edit_photoInput",
               "Select Photo", 
+              width = "700px",
               accept = c("image/jpeg", "image/png")
-    ),
-    column(4,
-           imageOutput("edit_previewImage", width = "100%", height = "65px")
     ),
     hidden(textInput("edit_course", label = "Course"),
            textInput("edit_buttons", label = "")
            )
-    )
    ),
   size = 'xl',
   easyClose = FALSE,
